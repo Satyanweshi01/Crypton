4c16199605c74a30df03191de286cf61a9dcd707
Rupak Bhabak <rbhabak108@gmail.com>
2026-07-27 12:07:16 +0530

    fix model.h

--- include/model.h
+++ include/model.h
@@ -7,22 +7,22 @@ typedef struct date_data
     int year;
 } date;
 
-typedef struct 
+typedef struct
 {
     int id;
     char service[50];
     char username[50];
     char password[50];
     char notes[200];
-    date updated_at
-    date created_at
+    date updated_at;
+    date created_at;
 } VaultEntry;
 
 typedef struct
 {
-   master_password_hash
-   date updated_at
-   date created_at
+    char master_password_hash[50];
+    date updated_at;
+    date created_at;
 } AppMetadata;
 
 #endif
